cda839477a610a98ea968b1d0a835c6932080073
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-11-10 15:38:44 +0800

    - About US update.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,50 @@
 import Header, { HeaderNoUser } from '@/components/Header';
 import { SidebarMenuSkeleton } from '@/components/ui/sidebar';
 import { useAuth } from '@clerk/nextjs';
-import Image from 'next/image';
 
 const About = () => {
+  const contents = {
+    ABOUT_US: `ITrack is a cutting-edge platform designed to empower IT students at Bulacan State University. By leveraging the power of data analytics, ITrack offers 		a comprehensive solution to help you:
+      Understand your strengths and weaknesses: Gain valuable insights into your skills and competencies.
+      Explore potential career paths: Discover exciting opportunities that align with your interests and goals.
+      Connect with industry professionals: Network with experienced IT experts and build valuable relationships.
+      Prepare for the future of work: Stay ahead of the curve with the latest industry trends and emerging technologies.
+      With ITrack, you’ll have the knowledge and resources to make informed decisions and achieve your full potential.`,
+    MISSION: `To empower IT students at Bulacan State University by providing a data-driven platform that fosters informed decision-making, career exploration, and 		professional development.`,
+    VISION: `To be the leading platform for IT students, equipping them with the knowledge and tools to excel in the ever-evolving technology landscape.`,
+  };
   return (
     <>
       <DynamicHeader />
-      <section className="w-3/4 pl-24 pt-32">
-        <h1 className="w-full font-geist-sans text-lg font-black duration-200 ease-in-out sm:text-xl md:text-3xl">
-          Check your career trajectory
-        </h1>
-        <h2 className="font-geist-sans text-base font-bold duration-200 ease-in-out sm:text-lg md:text-2xl">
-          anywhere and anytime.
-        </h2>
-        <p className="p-2 font-geist-mono text-xs duration-200 ease-in-out sm:text-sm">
-          Welcome to iTrack! Discover your ideal IT career path with data-driven
-          insights. Our platform helps Bustos Campus students find the right job
-          by analyzing your skills, education, and goals. Get personalized
-          recommendations and take the next step towards your future in IT!
-        </p>
-      </section>
-      <div className="absolute bottom-0 right-2">
-        <Image
-          priority
-          src="/about.png"
-          className="h-auto w-[1080px]"
-          width={1920}
-          height={1080}
-          alt=""
-        />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <section className="pl-12 pt-12 duration-200 ease-in-out sm:w-3/4">
+          <div className="from-itrack-primary to-itrack-primary/90 mb-12 rounded-lg bg-gradient-to-bl px-2 py-8 text-center shadow-sm duration-200 ease-in-out">
+            <h1 className="w-full font-geist-sans text-xl font-black text-black duration-200 ease-in-out sm:text-2xl md:text-4xl">
+              Check your career trajectory
+            </h1>
+            <h2 className="font-geist-sans text-lg font-bold text-black duration-200 ease-in-out sm:text-xl md:text-2xl">
+              anywhere and anytime.
+            </h2>
+          </div>
+          <div className="flex flex-col gap-12">
+            {Object.entries(contents).map(([key, content]) => {
+              const isAboutUs = key === 'ABOUT_US';
+
+              return (
+                <div key={key} className="flex flex-col gap-1">
+                  <h2
+                    className={`${isAboutUs ? 'text-2xl' : ''} text-center font-bold duration-300 ease-in-out`}
+                  >
+                    {key.replace(/_/, ' ')}
+                  </h2>
+                  <p className="p-2 text-justify font-geist-mono text-lg duration-200 ease-in-out">
+                    {content}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </section>
       </div>
     </>
   );
